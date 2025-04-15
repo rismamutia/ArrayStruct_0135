ff4f74e0a78f51047da4efe179baac697342bd0d
rismamutia <rismamutiadewi89@gmail.com>
2025-04-15 20:17:03 +0700

    menampilkan data struct

--- StructGetLine.cpp
+++ StructGetLine.cpp
@@ -15,3 +15,26 @@ struct mahasiswa
     detailAlamat alamat;
 };
 
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukkan NAMA = ";
+    getline(cin , mhs.nama);
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Desa = " << mhs.alamat.desa << endl;
+    cout << "Kota = " << mhs.alamat.kota << endl;
+
+}
