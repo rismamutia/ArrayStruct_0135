1a286710972e83815ae199d30b557af222642501
rismamutia <rismamutiadewi89@gmail.com>
2025-04-15 20:11:35 +0700

    menampilkan data struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,25 @@ struct mahasiswa
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20220140102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+
+}
