ce547ae62c4d95befd4cd34a97ad8fbc8f8765d2
rismamutia <rismamutiadewi89@gmail.com>
2025-04-15 20:03:25 +0700

    mendeklarasikan array tanpaa ukuraan dan dengan ukuran

---
+++ Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi array tanpa ukuran, wajib langsung diisi data
+int nilai[]={1,2,3};
+
+//deklarasi array dengan ukuran, dan diberi nilai awal
+int skor[4]={9,13,20,11};
+
+//deklarasi array dengan ukuran, tanpa nilai awal
+string nama[3];
