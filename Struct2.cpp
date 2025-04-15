ea5b4fa28f7bde180b3b8b2272525dc8491f6037
rismamutia <rismamutiadewi89@gmail.com>
2025-04-15 20:14:31 +0700

    menampilkan data struct

--- Struct2.cpp
+++ Struct2.cpp
@@ -6,3 +6,33 @@ struct detailAlamat
     string desa;
     string kota;
 };
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Desa = " << mhs.alamat.desa << endl;
+    cout << "Kota = " << mhs.alamat.kota << endl;
+
+}
