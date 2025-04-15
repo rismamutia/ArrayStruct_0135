4825467f49c6e0f8a0a372c949fb33fad356a5a0
rismamutia <rismamutiadewi89@gmail.com>
2025-04-15 20:16:12 +0700

    menampilkan fungsi struct

---
+++ StructGetLine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
